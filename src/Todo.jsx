f5560b03f8cfcaaddc57d94bfc5db8337a458a49
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-15 15:31:10 -0700

    last time

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-// import { ACTION } from './App.jsx'
+//import { ACTION } from './App.jsx'
 export default function Todo({todo}) {
   return (
     <div>
 
 
-      {/* <span style = {{color:todo.complete ? '#AAA' : '#000'}}>
+      <span style = {{color:todo.complete ? '#AAA' : '#000'}}>
         {todo.name}
-      </span> */}
+      </span>
 
 
 
-      {/* <button onClick={()=>dispatch({type: ACTION.TOGGLE_TODO,
+      <button onClick={()=>dispatch({type: ACTION.TOGGLE_TODO,
       payload: { id: todo.id }
 
       })}>  Disable</button>
       <button onClick={()=>dispatch({type: ACTION.DELETE_TODO,
       payload: { id: todo.id }
 
-      })}> Delete</button> */}
+      })}> Delete</button>
 
       <button>ADD</button>
     </div>
